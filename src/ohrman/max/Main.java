3b80928825b5143e433780b5e4147a70ca8b9f4c
Max Öhrman <Ohrman.max@gmail.com>
2023-03-27 21:08:51 +0200

    Added sellItem method

--- src/ohrman/max/Main.java
+++ src/ohrman/max/Main.java
@@ -18,6 +18,8 @@ public class Main {
 
         temp = new StockItem("cup", 0.5, 200);
         stockList.addStock(temp);
+        temp = new StockItem("cup", 0.5, 7);
+        stockList.addStock(temp);
 
         temp = new StockItem("door", 72.95, 4);
         stockList.addStock(temp);
@@ -36,5 +38,50 @@ public class Main {
 
         System.out.println(stockList);
 
+        for (String s: stockList.items().keySet()) {
+            System.out.printf(s);
+        }
+
+        //Test cases
+        Basket maxBasket = new Basket("Max");
+        sellItem(maxBasket, "car", 1);
+        System.out.println(maxBasket);
+
+        sellItem(maxBasket, "car", 1);
+        System.out.println(maxBasket);
+
+        sellItem(maxBasket, "car", 1);
+        sellItem(maxBasket, "spanner", 5);
+        System.out.println(maxBasket);
+
+        sellItem(maxBasket, "juice", 4);
+        sellItem(maxBasket, "cup", 12);
+        sellItem(maxBasket, "bread", 1);
+        System.out.println(maxBasket);
+
+        System.out.println(stockList);
+
+
+    }
+
+    /**
+     *
+     * @param basket - basket in which item is placed if exists
+     * @param item - the item we want to sell
+     * @param quantity - the amount of items
+     * @return - returns the quantity sold, can be 0.
+     */
+    public static int sellItem(Basket basket, String item, int quantity) {
+        //retrieve the item from stock list first
+        StockItem stockItem = stockList.get(item);
+        if(stockItem == null) {
+            System.out.println("We don't sell " + item);
+            return 0;
+        }
+        if(stockList.sellStock(item, quantity) !=0) {
+            basket.addToBasket(stockItem, quantity);
+            return quantity;
+        }
+        return 0;
     }
 }
